f17ca0c20978c9e46010ab4c0cbf82d9e05bc9b5
Colin Sheppard <colinthegaunt@gmail.com>
2016-04-08 23:19:45 -0700

    lot's of experimentation, changes, and fixes

--- model/R/autonomous-sim.R
+++ model/R/autonomous-sim.R
@@ -4,17 +4,19 @@ load(pp(ev.amod.shared,'model/inputs/devo-params.Rdata'))
 source(pp(ev.amod.model,'model/R/misc-functions.R'))
 
 animate.soln <- function(sys){
+  my.cat('animating')
+  sys[,var:=factor(var,c('u','sic','v','sid','w'))]
   for(node in u(sys$node)){
     image.i <- 1
     for(the.t in u(sys$t)){
       png(filename = pp(ev.amod.shared,'model/results/soln-node',node,'-img',sprintf('%05d',image.i),".png"),width=800,height=500)
-      p<-ggplot(sys[t==the.t],aes(x=x,y=val,colour=var))+geom_line()+facet_grid(var~.,scales='free_y')+labs(title=pp('t=',the.t))
+      p<-ggplot(sys[t==the.t],aes(x=x,y=val,colour=var))+geom_line()+facet_grid(var~.,scales='free_y')+labs(title=pp('t=',the.t))+scale_color_manual(values=c('black','red','black','red','black'))+scale_y_continuous(limits=range(sys$val))
       #p<-ggplot(sys[t==the.t],aes(x=x,y=val,colour=var))+geom_line()+facet_grid(var~.)+labs(title=pp('t=',the.t))+scale_y_continuous(limits=range(sys$val))
       print(p)
       dev.off()
       image.i <- image.i + 1
     }
-    #system(pp('ffmpeg -framerate 30 -i ',ev.amod.shared,'model/results/soln-node',node,'-img%05d.png -s:v 800x1000 -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p -r 30 ',ev.amod.shared,'model/results/soln-node',node,'.mp4'),intern=T,input='Y')
+    system(pp('ffmpeg -framerate 30 -i ',ev.amod.shared,'model/results/soln-node',node,'-img%05d.png -vf "scale=640:-1" -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p -r 30 ',ev.amod.shared,'model/results/soln-node',node,'.mp4'),intern=T,input='Y')
   }
 }
 
@@ -35,7 +37,7 @@ ev.amod.sim <- function(params){
   n.t <- length(ts)
   
   Qc <- function(x){ # SOE/min
-    #if(x==1)return(0)
+    if(x==1)return(0)
     params$ChargingRate*params$ChargingEfficiency/params$BatteryCapacity/60 # 60 converts from hour to min
   }
   uLW1 <- function(x){
@@ -45,7 +47,7 @@ ev.amod.sim <- function(params){
    (Qc(x)*params$dt/params$dx)^2/2
   }
   Qd <- function(x){ # SOE/min
-    #if(x==0)return(0)
+    if(x==0)return(0)
     -params$DischargingRate/params$BatteryCapacity/60 
   }
   wLW1 <- function(x){
@@ -91,22 +93,137 @@ ev.amod.sim <- function(params){
   for(ix in 1:length(xs)){
     for(inode in 1:length(nodes)){
       constr[i.constr,pp('u-i',nodes[inode],'-x',xs[ix],'-t0')] <- 1
-      rhs[i.constr] <- params$FleetSize/n.nodes/params$dx/length(xs)/3
-      #rhs[i.constr] <- ifelse(ix==3,0,params$FleetSize/n.nodes/length(xs)/3)
+      rhs[i.constr] <- 0
+      ## Constraints from Caroline's paper which I don't fully understand / agree with
+      #rhs[i.constr] <- ifelse(ix==1,0,params$FleetSize/n.nodes/3)
       i.constr <- i.constr + 1
       constr[i.constr,pp('v-i',nodes[inode],'-x',xs[ix],'-t0')] <- 1
-      rhs[i.constr] <- params$FleetSize/n.nodes/params$dx/length(xs)/3
-      #rhs[i.constr] <- ifelse(ix==2,0,params$FleetSize/n.nodes/length(xs)/3)
+      rhs[i.constr] <- params$FleetSize/n.nodes
       i.constr <- i.constr + 1
       constr[i.constr,pp('w-i',nodes[inode],'-x',xs[ix],'-t0')] <- 1
-      rhs[i.constr] <- params$FleetSize/n.nodes/params$dx/length(xs)/3
-      #rhs[i.constr] <- ifelse(ix==1,0,params$FleetSize/n.nodes/length(xs)/3)
+      rhs[i.constr] <- 0
+      ## Constraints from Caroline's paper which I don't fully understand / agree with
+      #rhs[i.constr] <- ifelse(ix==length(xs),0,params$FleetSize/n.nodes/3)
       i.constr <- i.constr + 1
     }
   }
   #######################
   # Equations of State
   #######################
+  ## Second order upwind
+  #n.constr <- 3*(n.t-1)*n.nodes*n.x 
+  #constr <- rbind(constr,array(0,c(n.constr,length(obj)),dimnames=list(pp('state',1:n.constr),names(obj))))
+  #rhs <- c(rhs,array(0,n.constr))
+  #for(it in 1:(length(ts)-1)){
+    #for(ix in 1:length(xs)){
+      #for(inode in 1:length(nodes)){
+        ## u: charging state 
+        #constr[i.constr,pp('u-i',nodes[inode],'-x',xs[ix],'-t',ts[it])]   <- 1-3*uLW1(xs[ix])
+        #if(ix>1){
+          #constr[i.constr,pp('u-i',nodes[inode],'-x',xs[ix-1],'-t',ts[it])] <- 4*uLW1(xs[ix])
+          #if(ix>2){
+            #constr[i.constr,pp('u-i',nodes[inode],'-x',xs[ix-2],'-t',ts[it])] <- -uLW1(xs[ix])
+          #}else{
+            ## use first order upwind 
+            #constr[i.constr,pp('u-i',nodes[inode],'-x',xs[ix],'-t',ts[it])]   <- 1-uLW1(xs[ix])*2 # 2 accounts for not dividing by 2dx
+            #constr[i.constr,pp('u-i',nodes[inode],'-x',xs[ix-1],'-t',ts[it])] <- uLW1(xs[ix])*2
+          #}
+        #}else{
+          ## use first order upwind with no upwind party
+          #constr[i.constr,pp('u-i',nodes[inode],'-x',xs[ix],'-t',ts[it])]   <- 1-uLW1(xs[ix])*2 # 2 accounts for not dividing by 2dx
+        #}
+        #constr[i.constr,pp('sic-i',nodes[inode],'-x',xs[ix],'-t',ts[it])]   <- params$dt
+        #constr[i.constr,pp('u-i',nodes[inode],'-x',xs[ix],'-t',ts[it+1])] <- -1
+        #i.constr <- i.constr + 1
+        ## v: idle state
+        #constr[i.constr,pp('v-i',nodes[inode],'-x',xs[ix],'-t',ts[it])]   <- 1
+        #constr[i.constr,pp('sic-i',nodes[inode],'-x',xs[ix],'-t',ts[it])]   <- -params$dt
+        #constr[i.constr,pp('sid-i',nodes[inode],'-x',xs[ix],'-t',ts[it])]   <- -params$dt
+        #constr[i.constr,pp('v-i',nodes[inode],'-x',xs[ix],'-t',ts[it+1])] <- -1
+        #i.constr <- i.constr + 1
+        ## w: discharging state
+        #constr[i.constr,pp('w-i',nodes[inode],'-x',xs[ix],'-t',ts[it])]   <- 1+3*wLW1(xs[ix])
+        #if(ix<length(xs)){
+          #constr[i.constr,pp('w-i',nodes[inode],'-x',xs[ix+1],'-t',ts[it])] <- -4*wLW1(xs[ix])
+          #if(ix<(length(xs)-1)){
+            #constr[i.constr,pp('w-i',nodes[inode],'-x',xs[ix+2],'-t',ts[it])] <- wLW1(xs[ix])
+          #}else{
+            ## use first order downwind 
+            #constr[i.constr,pp('w-i',nodes[inode],'-x',xs[ix],'-t',ts[it])] <- 1+wLW1(xs[ix])*2
+            #constr[i.constr,pp('w-i',nodes[inode],'-x',xs[ix+1],'-t',ts[it])] <- -wLW1(xs[ix])*2
+          #}
+        #}else{
+          ## use first order downwind with no downwind party
+          #constr[i.constr,pp('w-i',nodes[inode],'-x',xs[ix],'-t',ts[it])] <- 1+wLW1(xs[ix])*2
+        #}
+        #constr[i.constr,pp('sid-i',nodes[inode],'-x',xs[ix],'-t',ts[it])]   <- params$dt
+        #constr[i.constr,pp('w-i',nodes[inode],'-x',xs[ix],'-t',ts[it+1])] <- -1
+        #i.constr <- i.constr + 1
+      #}
+    #}
+  #}
+
+  # First order upwind
+  #n.constr <- 3*(n.t-1)*n.nodes*n.x 
+  #constr <- rbind(constr,array(0,c(n.constr,length(obj)),dimnames=list(pp('state',1:n.constr),names(obj))))
+  #rhs <- c(rhs,array(0,n.constr))
+  #for(it in 1:(length(ts)-1)){
+    #for(ix in 1:length(xs)){
+      #for(inode in 1:length(nodes)){
+        ## u: charging state 
+        #if(ix>1)constr[i.constr,pp('u-i',nodes[inode],'-x',xs[ix-1],'-t',ts[it])] <- uLW1(xs[ix])*2
+        #constr[i.constr,pp('u-i',nodes[inode],'-x',xs[ix],'-t',ts[it])]   <- 1-uLW1(xs[ix])*2
+        #constr[i.constr,pp('sic-i',nodes[inode],'-x',xs[ix],'-t',ts[it])]   <- params$dt
+        #constr[i.constr,pp('u-i',nodes[inode],'-x',xs[ix],'-t',ts[it+1])] <- -1
+        #i.constr <- i.constr + 1
+        ## v: idle state
+        #constr[i.constr,pp('v-i',nodes[inode],'-x',xs[ix],'-t',ts[it])]   <- 1
+        #constr[i.constr,pp('sic-i',nodes[inode],'-x',xs[ix],'-t',ts[it])]   <- -params$dt
+        #constr[i.constr,pp('sid-i',nodes[inode],'-x',xs[ix],'-t',ts[it])]   <- -params$dt
+        #constr[i.constr,pp('v-i',nodes[inode],'-x',xs[ix],'-t',ts[it+1])] <- -1
+        #i.constr <- i.constr + 1
+        ## w: discharging state
+        #constr[i.constr,pp('w-i',nodes[inode],'-x',xs[ix],'-t',ts[it])]   <- 1+wLW1(xs[ix])*2
+        #if(ix<length(xs))constr[i.constr,pp('w-i',nodes[inode],'-x',xs[ix+1],'-t',ts[it])] <- -wLW1(xs[ix])*2
+        #constr[i.constr,pp('sid-i',nodes[inode],'-x',xs[ix],'-t',ts[it])]   <- params$dt
+        #constr[i.constr,pp('w-i',nodes[inode],'-x',xs[ix],'-t',ts[it+1])] <- -1
+        #i.constr <- i.constr + 1
+      #}
+    #}
+  #}
+
+  ## lax wendroff with 1st order downwind at boundary
+  #n.constr <- 3*(n.t-1)*n.nodes*n.x 
+  #constr <- rbind(constr,array(0,c(n.constr,length(obj)),dimnames=list(pp('state',1:n.constr),names(obj))))
+  #rhs <- c(rhs,array(0,n.constr))
+  #for(it in 1:(length(ts)-1)){
+    #for(ix in 1:length(xs)){
+      #for(inode in 1:length(nodes)){
+        ## u: charging state 
+        #if(ix>1)constr[i.constr,pp('u-i',nodes[inode],'-x',xs[ix-1],'-t',ts[it])] <- uLW1(xs[ix])+uLW2(xs[ix])
+        #constr[i.constr,pp('u-i',nodes[inode],'-x',xs[ix],'-t',ts[it])]   <- 1-2*uLW2(xs[ix])
+        #if(ix<length(xs))constr[i.constr,pp('u-i',nodes[inode],'-x',xs[ix+1],'-t',ts[it])] <- uLW2(xs[ix])-uLW1(xs[ix])
+        #constr[i.constr,pp('sic-i',nodes[inode],'-x',xs[ix],'-t',ts[it])]   <- params$dt
+        #constr[i.constr,pp('u-i',nodes[inode],'-x',xs[ix],'-t',ts[it+1])] <- -1
+        #i.constr <- i.constr + 1
+        ## v: idle state
+        #constr[i.constr,pp('v-i',nodes[inode],'-x',xs[ix],'-t',ts[it])]   <- 1
+        #constr[i.constr,pp('sic-i',nodes[inode],'-x',xs[ix],'-t',ts[it])]   <- -params$dt
+        #constr[i.constr,pp('sid-i',nodes[inode],'-x',xs[ix],'-t',ts[it])]   <- -params$dt
+        #constr[i.constr,pp('v-i',nodes[inode],'-x',xs[ix],'-t',ts[it+1])] <- -1
+        #i.constr <- i.constr + 1
+        ## w: discharging state
+        #if(ix>1)constr[i.constr,pp('w-i',nodes[inode],'-x',xs[ix-1],'-t',ts[it])] <- wLW1(xs[ix])+wLW2(xs[ix])
+        #constr[i.constr,pp('w-i',nodes[inode],'-x',xs[ix],'-t',ts[it])]   <- 1-2*wLW2(xs[ix])
+        #if(ix<length(xs))constr[i.constr,pp('w-i',nodes[inode],'-x',xs[ix+1],'-t',ts[it])] <- wLW2(xs[ix])-wLW1(xs[ix])
+        #constr[i.constr,pp('sid-i',nodes[inode],'-x',xs[ix],'-t',ts[it])]   <- params$dt
+        #constr[i.constr,pp('w-i',nodes[inode],'-x',xs[ix],'-t',ts[it+1])] <- -1
+        #i.constr <- i.constr + 1
+      #}
+    #}
+  #}
+
+  ## Below are lax wendroff
   n.constr <- 3*(n.t-1)*n.nodes*n.x 
   constr <- rbind(constr,array(0,c(n.constr,length(obj)),dimnames=list(pp('state',1:n.constr),names(obj))))
   rhs <- c(rhs,array(0,n.constr))
@@ -139,7 +256,7 @@ ev.amod.sim <- function(params){
   #######################
   # Boundary Conditions
   #######################
-  n.constr <- 2*n.nodes*n.t
+  n.constr <- 4*n.nodes*n.t
   #n.constr <- n.nodes*n.t
   constr <- rbind(constr,array(0,c(n.constr,length(obj)),dimnames=list(pp('bound',1:n.constr),names(obj))))
   rhs <- c(rhs,array(0,n.constr))
@@ -155,9 +272,9 @@ ev.amod.sim <- function(params){
       i.constr <- i.constr + 1
     }
   }
-  ########################
-  ## Total number of vehicles
-  ########################
+  #######################
+  # Total number of vehicles
+  #######################
   #n.constr <- 1*n.nodes*n.t
   #constr <- rbind(constr,array(0,c(n.constr,length(obj)),dimnames=list(pp('tot.num',1:n.constr),names(obj))))
   #rhs <- c(rhs,array(0,n.constr))
@@ -170,7 +287,7 @@ ev.amod.sim <- function(params){
         #constr[i.constr,pp('w-i',nodes[inode],'-x',xs[ix],'-t',ts[it])] <- 1 
       #}
     #}
-    #rhs[i.constr] <- params$FleetSize
+    #rhs[i.constr] <- params$FleetSize/params$dx
     #i.constr <- i.constr + 1
   #}
   #######################
@@ -228,19 +345,20 @@ ev.amod.sim <- function(params){
   ########################
   ## Discharging limit -- should be less or equal to the load demand
   ########################
-  #n.constr <- 1*n.nodes*n.x*n.t
-  #constr.ineq <- rbind(constr.ineq, array(0,c(n.constr,length(obj)),dimnames=list(pp('dc.lim',1:n.constr),names(obj))))
-  #rhs.ineq <- c(rhs.ineq,array(0,n.constr))
-  #for(inode in 1:length(nodes)){
-    #for(it in 1:length(ts)){
-      #for(ix in 1:length(xs)){ 
-        #constr.ineq[i.constr.ineq,pp('w-i',nodes[inode],'-x',xs[ix],'-t',ts[it])] <- (-1)*Qd(xs[ix])*params$BatteryCapacity/params$dt
-      #}
-      #rhs.ineq[i.constr.ineq] <- ifelse(length(which((load[,time] == ts[it] & load[,node] == nodes[inode])=='TRUE',arr.ind=T)) >= 1,load[time == ts[it] & node == nodes[inode], demand],0)
-      ##my.cat(rhs.ineq[i.constr.ineq])
-      #i.constr.ineq <- i.constr.ineq + 1
-    #}
-  #}
+  n.constr <- 1*n.nodes*n.x*n.t
+  constr.ineq <- rbind(constr.ineq, array(0,c(n.constr,length(obj)),dimnames=list(pp('dc.lim',1:n.constr),names(obj))))
+  rhs.ineq <- c(rhs.ineq,array(0,n.constr))
+  for(inode in 1:length(nodes)){
+    the.load <- load[node==nodes[inode]]
+    for(it in 1:length(ts)){
+      for(ix in 1:length(xs)){ 
+        constr.ineq[i.constr.ineq,pp('w-i',nodes[inode],'-x',xs[ix],'-t',ts[it])] <- 1
+      }
+      rhs.ineq[i.constr.ineq] <- the.load[findInterval(ts[it],the.load[,time])]$demand/params$DischargingRate/params$dx
+      #my.cat(rhs.ineq[i.constr.ineq])
+      i.constr.ineq <- i.constr.ineq + 1
+    }
+  }
   
   #------------------------------------------ End Contraints --------------------------------------------------#
   
@@ -273,7 +391,6 @@ ev.amod.sim <- function(params){
   my.cat(pp("result: ",status.code(status)))
 
   if(sol$status==0){
-    my.cat('animating')
     soln <- sol$solution
     names(soln) <- names(obj)
     sol$sys <- parse.results(soln)
@@ -284,20 +401,23 @@ ev.amod.sim <- function(params){
   return(sol)
 }
 
-params$T <- 50
+params$T <- 75
 params$dx <- 0.05
-params$dt <- 2
-params$FleetSize <- 330
-params$NodeFile <- pp(ev.amod.shared,'model/inputs/tiny/nodes.csv')
+params$dt <- 5
+params$FleetSize <- 300
+params$NodeFile <- pp(ev.amod.shared,'model/inputs/tiny/nodes-2.csv')
+ptm <- proc.time()
 sol <- ev.amod.sim(params)
+print(proc.time() - ptm)
 sys <- sol$sys
 animate.soln(sol$sys)
 
 # Debugging
 
 # Check for conservation of vehicles
-setkey(sys,t,var)
-sys[-grep('si',var),list(sum(val*params$dx)),by=c('t')]
+setkey(sys,t,var,x)
+sys[-grep('si',var)][,list(count=sum((head(val,-1)+tail(val,-1))/2*params$dx)),by=c('t','var')][,list(count=sum(count)),by='t']
+
 #sys[t<=5,list(round(sum(val*params$dx),1)),by=c('t','var')]
 #sys[x>=0.9 & t<=5,list(round(sum(val*params$dx),1)),by=c('t','x','var')]
 
